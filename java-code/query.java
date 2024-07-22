8fc10bd864e75c7750518b00a2f2f8e3442dbf65
Shivam Lad <shivamlad29@gmail.com>
2024-07-22 14:59:38 +0530

    update java vulnerable code

--- java-code/query.java
+++ java-code/query.java
@@ -10,6 +10,7 @@ String query = "SELECT secret FROM Users WHERE (username = '" + username + "' AN
 // Insecurely format the query string using user defined data 
 String query = String.format("SELECT secret FROM Users WHERE (username = '%s' AND NOT role = 'admin')", username);
 // Execute query and return the results
-ResultSet result = statement.executeQuery(query);  {
+ResultSet result = statement.executeQuery(query);  
+{
     
 }
